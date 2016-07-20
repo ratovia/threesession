62d8ee2558b525ba631dbbd458276c7977c95087
ratovia <ratovia@hotmail.co.jp>
2016-07-20 20:41:10 +0900

    [hotfix] menubar
    dropdown and dropup menu fixed

--- js/Menu.js
+++ js/Menu.js
@@ -1,3 +1,27 @@
 THREESESSION.Menu = function(parameters){
+	$(".view-menu-trigger").hover(
+	  function () {
+	    $("#view-menu").show();
+	  },
+	  function () {
+	    $("#view-menu").hide();
+	  }
+  );
+	$(".add-menu-trigger").hover(
+	  function () {
+	    $("#add-menu").show();
+	  },
+	  function () {
+	    $("#add-menu").hide();
+	  }
+  );
 
+	$(".file-menu-trigger").hover(
+	  function () {
+	    $("#file-menu").show();
+	  },
+	  function () {
+	    $("#file-menu").hide();
+	  }
+  );
 }
