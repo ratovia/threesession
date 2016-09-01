f298a937186677d2c307c402392c1367424e5ff4
ratovia <ratovia@hotmail.co.jp>
2016-09-01 13:24:58 +0900

    [add] session
    session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def index
-    @user_name = params[:user_name];
+    @user_name = params[:user_name]
+    @user = User.create(name: @user_name)
+    session[:user_id] = @user.id
   end
 end
