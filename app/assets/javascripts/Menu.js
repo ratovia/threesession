5f2f671cb3eea79730b407849d7f1cee58233f88
ratovia <ratovia@hotmail.co.jp>
2019-07-27 02:07:14 +0900

    update toggle menu

--- app/assets/javascripts/Menu.js
+++ app/assets/javascripts/Menu.js
@@ -1,47 +1,29 @@
-THREESESSION.Menu = function(parameters){
-	$(".view-menu-trigger").hover(
-	  function () {
-	    $("#view-menu").show();
-	  },
-	  function () {
-	    $("#view-menu").hide();
-	  }
-  );
-	$(".add-menu-trigger").hover(
-	  function () {
-	    $("#add-menu").show();
-	  },
-	  function () {
-	    $("#add-menu").hide();
-	  }
-  );
-
-	$(".file-menu-trigger").hover(
-	  function () {
-	    $("#file-menu").show();
-	  },
-	  function () {
-	    $("#file-menu").hide();
-	  }
-	);
-
+THREESESSION.Menu = function(){
 	$(".toggle-menu").on("click",function(){
-		if ($('#side_bar').hasClass(".menu-open")){
+		if ($('#side_bar').hasClass("menu-open")){
 			$('#side_bar').css("left","-200px");
-			$('#side_bar').removeClass(".menu-open")
+			$('#side_bar').removeClass("menu-open")
 		} else {
 			$('#side_bar').css("left","0px");
-			$('#side_bar').addClass(".menu-open")
+			$('#side_bar').addClass("menu-open")
 		}
-	})
+	});
 	$(".toggle-list-menu").on("click",function(){
-		let child = $(this).children("ul")
-		if (child.hasClass(".menu-open")){
+		let child = $(this).children("ul");
+		if(child.hasClass("menu-open")){
 			child.css("display","none");
-			child.removeClass(".menu-open")
-		} else {
+			child.removeClass("menu-open");
+		}else{
 			child.css("display","block");
-			child.addClass(".menu-open");
+			child.addClass("menu-open");
 		}
-	})
+	});
+	$(".toggle-operations-menu").on("mouseover",function(){
+		let child = $(this).children("ul")
+		child.css("display","block");
+	});
+	$(".toggle-operations-menu").on("mouseout",function(){
+		let child = $(this).children("ul")
+		child.css("display","none");
+	});
 }
