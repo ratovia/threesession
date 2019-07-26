02b768144f797b32d55b69cdc23b1397e599b894
ratovia <ratovia@hotmail.co.jp>
2019-07-26 23:28:18 +0900

    add toggle menu

--- app/assets/javascripts/Menu.js
+++ app/assets/javascripts/Menu.js
@@ -23,5 +23,25 @@ THREESESSION.Menu = function(parameters){
 	  function () {
 	    $("#file-menu").hide();
 	  }
-  );
+	);
+
+	$(".toggle-menu").on("click",function(){
+		if ($('#side_bar').hasClass(".menu-open")){
+			$('#side_bar').css("left","-200px");
+			$('#side_bar').removeClass(".menu-open")
+		} else {
+			$('#side_bar').css("left","0px");
+			$('#side_bar').addClass(".menu-open")
+		}
+	})
+	$(".toggle-list-menu").on("click",function(){
+		let child = $(this).children("ul")
+		if (child.hasClass(".menu-open")){
+			child.css("display","none");
+			child.removeClass(".menu-open")
+		} else {
+			child.css("display","block");
+			child.addClass(".menu-open");
+		}
+	})
 }
